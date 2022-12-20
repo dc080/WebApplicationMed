e739d4947a0e246114a54174a2e6071b97dfa959
dc080 <danilche080@gmail.com>
2022-12-20 15:38:10 +0600

    загрузка изображений

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'styles/image'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///med.db'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 
-class Article(db.Model):
-    id = db.Columd(db.Integer, primary_key=True)
-    picture = db.Columd(db.Integer)
+
+# class Article(db.Model):
+#    id = db.Columd(db.Integer, primary_key=True)
+#   picture = db.Columd(db.Integer)
 
 
-
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template("index.html")
     return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
