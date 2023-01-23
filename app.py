c66703da201b9adf406e9bc42d6eb8550cf2c167
dc080 <danilche080@gmail.com>
2023-01-23 17:09:41 +0600

    Рабочий вариант с отображением только загруженного изображения есть обработка неверных запросов, сообщений об ошибке пока нет для запуска на сервере изменить адресс app.py (app.run(host='0.0.0.0')) в main pl заменить на plt

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, request, g, url_for, redirect
+from flask import Flask, render_template, request, g, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import os
-
-# import main
 from main import Analysis
 
 UPLOAD_FOLDER = 'static/image'
@@ -12,27 +10,18 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///med.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 db = SQLAlchemy(app)
 
 uploaded_image = ''
 pic = UPLOAD_FOLDER + '/' + uploaded_image
 
-# pic = 'static/image/pic.png'
 answer = Analysis()
 answerS = Analysis()
 
 
-
-
-# , Res=answer.FractAnalysis(pic), S=answerS.SquareNLinesAnalysis(pic, light, dark)) - Вывод в алгоритм
-
-
-# class Article(db.Model):
-#    id = db.Columd(db.Integer, primary_key=True)
-#   picture = db.Columd(db.Integer)
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # return render_template('index.html', Res=answer.FractAnalysis(pic))  # answer.SquareNLinesAnalysis(pic, light, dark) +
     if request.method == "POST":
         try:
             file = request.files['file']
@@ -45,23 +34,39 @@ def index():
             uploaded_image = file.filename
             pic = UPLOAD_FOLDER + '/' + uploaded_image
             return render_template("index.html", pic=UPLOAD_FOLDER + '/' + uploaded_image, uploaded_image=file.filename,
-                                   contours="static/image/tempimg.jpg", light=request.form['light'],
+                                   light=request.form['light'],
                                    dark=request.form['dark'],
                                    Res=answer.FractAnalysis(pic), S=answerS.SquareNLinesAnalysis(pic, light, dark))
         except FileNotFoundError:
+            flash('Выберите изображение')
             return render_template("index.html")
+        except IsADirectoryError:
+            flash('Выберите изображение')
+            return render_template("index.html")
+        except ValueError:
+            flash('Некорректное значение границ')
+            return render_template("index.html")
+
+
     else:
+        files = os.listdir(UPLOAD_FOLDER)
+        if len(files) > 50:
+            for f in os.listdir(UPLOAD_FOLDER):
+                os.remove(os.path.join(UPLOAD_FOLDER, f))
         return render_template("index.html")
 
 
-#@app.route('/number', methods=['GET', 'POST'])
-#def number():
- #   if request.method == 'POST':
-  #      Border['light'] = request.form['light']
-   #     Border['dark'] = request.form['dark']
-    #    return render_template("index.html", light=Border['light'], dark=Border['dark'])
-   # else:
-    #    return render_template("index.html")
+if __name__ == "__main__":
+    app.run(debug=False)
+
+# @app.route('/number', methods=['GET', 'POST'])
+# def number():
+#   if request.method == 'POST':
+#      Border['light'] = request.form['light']
+#     Border['dark'] = request.form['dark']
+#    return render_template("index.html", light=Border['light'], dark=Border['dark'])
+# else:
+#    return render_template("index.html")
 
 
 # @app.route('/load', methods=['POST'])
@@ -75,6 +80,6 @@ def index():
 #                         S=answerS)
 # else:
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# class Article(db.Model):
+#    id = db.Columd(db.Integer, primary_key=True)
+#   picture = db.Columd(db.Integer)
