7edc078db4bf5053bdf26a25b4d1bd5fa32cf2b5
dc080 <danilche080@gmail.com>
2023-02-06 13:33:32 +0600

    Изображение если файл не найден, исправлена очистка папки сесии

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from flask_session import Session
 import os
 
 # import main
-from Algorithms.FindFractNumber import FindFractNumber # Библиотека для фрактального числа
-from Algorithms.FindSquareCrystals import FindSquareCrystals # Библиотека для поиска площади кристаллов
+from Algorithms.FindFractNumber import FindFractNumber  # Библиотека для фрактального числа
+from Algorithms.FindSquareCrystals import FindSquareCrystals  # Библиотека для поиска площади кристаллов
 
 UPLOAD_FOLDER = 'static/image'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -18,56 +18,58 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///med.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SECRET_KEY'] = 'someRandomKey' # Какой-то секретный ключ, без которого нихера не работает
-app.config['SESSION_TYPE'] = 'filesystem' # Тип подключение сессии
+app.config['SECRET_KEY'] = 'someRandomKey'  # Какой-то секретный ключ, без которого нихера не работает
+app.config['SESSION_TYPE'] = 'filesystem'  # Тип подключение сессии
 
-Session(app) # Запуск сессии в приложении
+Session(app)  # Запуск сессии в приложении
 
 db = SQLAlchemy(app)
 
 algoFract = FindFractNumber()
 algoSquare = FindSquareCrystals()
 
-def verifySessionId(): # Функция для определения id
+
+def verifySessionId():  # Функция для определения id
     ###################################################################
-    session.clear() # ТЕСТОВАЯ ХЕРНЯ КОТОРУЮ НАДО УДАЛИТЬ ПОСЛЕ ОТЛАДКИ
+    # session.clear() # ТЕСТОВАЯ ХЕРНЯ КОТОРУЮ НАДО УДАЛИТЬ ПОСЛЕ ОТЛАДКИ
     ###################################################################
-    if not 'dateSingIn' in session: # Если в хэше нет записи о дате
-        session.clear() # Очищает весь хэш сессии
-    elif (datetime.datetime.now() - session['dateSingIn']).days > 1: # Или если дата есть, но у нее разница с текущей
+    if not 'dateSingIn' in session:  # Если в хэше нет записи о дате
+        session.clear()  # Очищает весь хэш сессии
+    elif (datetime.datetime.now() - session['dateSingIn']).days > 1:  # Или если дата есть, но у нее разница с текущей
         # больше дня
-        session.clear() # Очищает весь хэш сессии
-    if not 'idSession' in session: # Проверка на отсутствие в хэше id сессии
-        while True: # Цикл для генерации не занятого значения
-            randomId = random.randint(1, 200) # Генерация рандомного числа от 1 до 200
-            if not os.path.exists(calculateSavePath(randomId)): # Если пути с этим числом не существует
-                break # то выход из цикла
-        session['idSession'] = randomId # Присвоение этого числа в хэш
-        session['dateSingIn'] = datetime.datetime.now() # Запись даты присвоения в хэш
-    return session['idSession'] # Возврат id, или нового, или старого не измененного
+        session.clear()  # Очищает весь хэш сессии
+    if not 'idSession' in session:  # Проверка на отсутствие в хэше id сессии
+        while True:  # Цикл для генерации не занятого значения
+            randomId = random.randint(1, 200)  # Генерация рандомного числа от 1 до 200
+            if not os.path.exists(calculateSavePath(randomId)):  # Если пути с этим числом не существует
+                break  # то выход из цикла
+        session['idSession'] = randomId  # Присвоение этого числа в хэш
+        session['dateSingIn'] = datetime.datetime.now()  # Запись даты присвоения в хэш
+    return session['idSession']  # Возврат id, или нового, или старого не измененного
 
 
-def calculateSavePath(id): # Функция расчета папки с использованием id
-    return UPLOAD_FOLDER + '/' + str(id) # Базовая папка + папка с названием id
+def calculateSavePath(id):  # Функция расчета папки с использованием id
+    return UPLOAD_FOLDER + '/' + str(id)  # Базовая папка + папка с названием id
 
 
-def calculatePathToSavedFile(id): # Функция возврата пути к загруженному файлу с использованием id,
+def calculatePathToSavedFile(id):  # Функция возврата пути к загруженному файлу с использованием id,
     # и типом файла
-    return UPLOAD_FOLDER + '/' + str(id) + '/input' + session['fileExtension'] # Базовая папка + папка с названием id + тип файла
+    return UPLOAD_FOLDER + '/' + str(id) + '/input' + session[
+        'fileExtension']  # Базовая папка + папка с названием id + тип файла
 
 
-def calculatePathToEditedFile(id): # Функция возврата пути к файлу, созданному алгоритмом с использованием id,
+def calculatePathToEditedFile(id):  # Функция возврата пути к файлу, созданному алгоритмом с использованием id,
     # и типом файла
-    return UPLOAD_FOLDER + '/' + str(id) + '/tempimg.jpg' # Базовая папка + папка с названием id + измененный файл
+    return UPLOAD_FOLDER + '/' + str(id) + '/tempimg.jpg'  # Базовая папка + папка с названием id + измененный файл
 
 
 def renameFile(filename, id):
-    fileName, fileExtension = os.path.splitext(filename) # Разбиение файла на его путь с названием и типом файла
-    session['fileExtension'] = fileExtension # Сохранение расширения в кеш
-    pathToNewFile = calculatePathToSavedFile(id) # Расчет нового пути к переименованному файлу
-    if os.path.isfile(pathToNewFile): # Проверка на существующий файл, в который хотим переименовывать
-        os.remove(pathToNewFile) # Удаляем если есть
-    os.rename(filename, pathToNewFile) # Переименование загруженного файла на файл с
+    fileName, fileExtension = os.path.splitext(filename)  # Разбиение файла на его путь с названием и типом файла
+    session['fileExtension'] = fileExtension  # Сохранение расширения в кеш
+    pathToNewFile = calculatePathToSavedFile(id)  # Расчет нового пути к переименованному файлу
+    if os.path.isfile(pathToNewFile):  # Проверка на существующий файл, в который хотим переименовывать
+        os.remove(pathToNewFile)  # Удаляем если есть
+    os.rename(filename, pathToNewFile)  # Переименование загруженного файла на файл с
     # необходимым названием и исходным типом файла
 
 
@@ -75,69 +77,93 @@ def renameFile(filename, id):
 def index():
     if request.method == "POST":
         try:
-            if "load" in request.form: # Если нажата кнопка загрузки
-                idSession = verifySessionId() # Присвоение ID, если это надо
-                file = request.files['file'] # Запрос файла
-                savePath = calculateSavePath(idSession) # Обозначение пути до загруженного файла
-                if not os.path.exists(savePath): # Если пути нет
-                    os.makedirs(savePath) # Создать его
-                savedFile = os.path.join(savePath, file.filename) # Путь до сохраняемого файла с его исходным названием
-                file.save(savedFile) # Сохранение файла
-                renameFile(savedFile, idSession) # Переименовка файла
-                pic = calculatePathToSavedFile(idSession) # Путь до сохраненного файла с новым названием
-                algoSquare.loadImage(pic) # Загрузка изображения в алгоритм
-                if 'squareNumber' in session: # Очистка результатов прошлых изображений
-                    session.pop('squareNumber') # Удалить результат
-                if 'fractalNumber' in session: # Очистка результатов прошлых изображений
-                    session.pop('fractalNumber') # Удалить результат
-                session['fractalNumber'] = algoFract.resolveFractNumber(pic) # Сохранение результата алгоритма в хэш
+            if "load" in request.form:  # Если нажата кнопка загрузки
+                idSession = verifySessionId()  # Присвоение ID, если это надо
+                files = os.listdir(UPLOAD_FOLDER + '/' + str(idSession))
+                if len(files) >= 1:
+                    shutil.rmtree(UPLOAD_FOLDER + '/' + str(idSession))
+                file = request.files['file']  # Запрос файла
+                savePath = calculateSavePath(idSession)  # Обозначение пути до загруженного файла
+                if not os.path.exists(savePath):  # Если пути нет
+                    os.makedirs(savePath)  # Создать его
+                savedFile = os.path.join(savePath, file.filename)  # Путь до сохраняемого файла с его исходным названием
+                file.save(savedFile)  # Сохранение файла
+                renameFile(savedFile, idSession)  # Переименовка файла
+                pic = calculatePathToSavedFile(idSession)  # Путь до сохраненного файла с новым названием
+                algoSquare.loadImage(pic)  # Загрузка изображения в алгоритм
+                if 'squareNumber' in session:  # Очистка результатов прошлых изображений
+                    session.pop('squareNumber')  # Удалить результат
+                if 'fractalNumber' in session:  # Очистка результатов прошлых изображений
+                    session.pop('fractalNumber')  # Удалить результат
+                session['fractalNumber'] = algoFract.resolveFractNumber(pic)  # Сохранение результата алгоритма в хэш
                 return render_template("index.html", uploaded_image=file.filename, contours=pic,
-                                   Res=session['fractalNumber'], light=algoSquare.light, dark=algoSquare.dark) # Отрисовка веб-страницы
+                                       Res=session['fractalNumber'], light=algoSquare.light,
+                                       dark=algoSquare.dark)  # Отрисовка веб-страницы
 
-            elif "edit" in request.form: # Если нажата кнопка изменения
-                light = int(request.form['light']) # Считывание значения ползунков
-                dark = int(request.form['dark']) # -/-
-                algoSquare.light = light # Занесение значений в алгоритм
-                algoSquare.dark = dark # -/-
-                algoSquare.editImage() # Обработка изображения
+            elif "edit" in request.form:  # Если нажата кнопка изменения
+                light = int(request.form['light'])  # Считывание значения ползунков
+                dark = int(request.form['dark'])  # -/-
+                algoSquare.light = light  # Занесение значений в алгоритм
+                algoSquare.dark = dark  # -/-
+                algoSquare.editImage()  # Обработка изображения
                 if 'squareNumber' in session:
                     session.pop('squareNumber')
-                return render_template("index.html", contours=calculatePathToEditedFile(session['idSession']), light=request.form['light'],
-                                   dark=request.form['dark'], Res=session['fractalNumber']) # Отрисовка веб-страницы
-
-            elif "runAlgo" in request.form: # Если нажата кнопка запуска алгоритма
-                light = int(request.form['light']) # Считывание значения ползунков
-                dark = int(request.form['dark']) # -/-
-                algoSquare.light = light # Занесение значений в алгоритм
-                algoSquare.dark = dark # -/-
-                algoSquare.editImage() # Обработка изображения
-                session['squareNumber'] = algoSquare.resolveSquare() # Занесение значения площади в хэш
                 return render_template("index.html", contours=calculatePathToEditedFile(session['idSession']),
-                                   light=request.form['light'], dark=request.form['dark'],
-                                   Res=session['fractalNumber'], S=session['squareNumber']) # Отрисовка веб-страницы
-            elif "orig" in request.form: # Если нажата "радио"кнопка оригинала
-                savePath = calculatePathToSavedFile(session['idSession']) # Путь к оригинальному файлу
-                if not 'fractalNumber' in session and not 'squareNumber' in session: # Если нет результатов
-                    return render_template("index.html", contours=savePath, light=algoSquare.light, # Рендер без результатов
-                                           dark=algoSquare.dark)
-                elif not 'squareNumber' in session: # Если нет только площадей
-                    return render_template("index.html", contours=savePath, light=algoSquare.light,
-                                           dark=algoSquare.dark, Res=session['fractalNumber']) # Рендер с фрактальной
-                else: # Есть все результаты
-                    return render_template("index.html", contours=savePath, light=algoSquare.light,
-                                           dark=algoSquare.dark, Res=session['fractalNumber'],
-                                           S=session['squareNumber']) # Есть все результаты, рендер их
+                                       light=request.form['light'],
+                                       dark=request.form['dark'],
+                                       Res=session['fractalNumber'])  # Отрисовка веб-страницы
 
-            elif "modifed" in request.form: # Если нажата "радио"кнопка оригинала
-                showPath = calculatePathToEditedFile(session['idSession']) # Путь к измененному файлу
-                if os.path.isfile(showPath): # Если этот файл существует (ПОЧЕМУ-ТО НЕ РАБОТАЕТ)
-                    showPath = calculatePathToSavedFile['idSession'] # Путь к оригинальному файлу
-                if not 'squareNumber' in session: # Если нет площадей
-                    return render_template("index.html", contours=showPath, light=algoSquare.light,
-                                            dark=algoSquare.dark, Res=session['fractalNumber'])  # Рендер с фрактальной
-                else: # Есть все результаты
-                    return render_template("index.html", contours=showPath, light=algoSquare.light, dark=algoSquare.dark,
-                                            Res=session['fractalNumber'], S=session['squareNumber']) # Есть все результаты, рендер их
+            elif "runAlgo" in request.form:  # Если нажата кнопка запуска алгоритма
+                light = int(request.form['light'])  # Считывание значения ползунков
+                dark = int(request.form['dark'])  # -/-
+                algoSquare.light = light  # Занесение значений в алгоритм
+                algoSquare.dark = dark  # -/-
+                algoSquare.editImage()  # Обработка изображения
+                session['squareNumber'] = algoSquare.resolveSquare()  # Занесение значения площади в хэш
+                return render_template("index.html", contours=calculatePathToEditedFile(session['idSession']),
+                                       light=request.form['light'], dark=request.form['dark'],
+                                       Res=session['fractalNumber'],
+                                       S=session['squareNumber'])  # Отрисовка веб-страницы
+            elif "orig" in request.form:  # Если нажата "радио"кнопка оригинала
+                files = os.listdir(UPLOAD_FOLDER + '/' + str((session['idSession'])))
+                if len(files) > 0:
+                    savePath = calculatePathToSavedFile(session['idSession'])  # Путь к оригинальному файлу
+                    if not 'fractalNumber' in session and not 'squareNumber' in session:  # Если нет результатов
+                        return render_template("index.html", contours=savePath, light=algoSquare.light,
+                                               # Рендер без результатов
+                                               dark=algoSquare.dark)
+                    elif not 'squareNumber' in session:  # Если нет только площадей
+                        return render_template("index.html", contours=savePath, light=algoSquare.light,
+                                               dark=algoSquare.dark,
+                                               Res=session['fractalNumber'])  # Рендер с фрактальной
+                    else:  # Есть все результаты
+                        return render_template("index.html", contours=savePath, light=algoSquare.light,
+                                               dark=algoSquare.dark, Res=session['fractalNumber'],
+                                               S=session['squareNumber'])  # Есть все результаты, рендер их
+                else:
+                    return render_template("index.html", contours="/static/static_image/not_found.png",
+                                           light=algoSquare.light,
+                                           dark=algoSquare.dark, )
+
+            elif "modifed" in request.form:  # Если нажата "радио"кнопка оригинала
+                files = os.listdir(UPLOAD_FOLDER + '/' + str((session['idSession'])))
+                if len(files) > 1:
+                    showPath = calculatePathToEditedFile(session['idSession'])  # Путь к измененному файлу
+                    if not 'squareNumber' in session:  # Если нет площадей
+                        return render_template("index.html", contours=showPath, light=algoSquare.light,
+                                               dark=algoSquare.dark,
+                                               Res=session['fractalNumber'])  # Рендер с фрактальной
+                    else:  # Есть все результаты
+                        return render_template("index.html", contours=showPath, light=algoSquare.light,
+                                               dark=algoSquare.dark,
+                                               Res=session['fractalNumber'],
+                                               S=session['squareNumber'])  # Есть все результаты, рендер их
+                else:
+                    return render_template("index.html", contours="/static/static_image/not_found.png",
+                                           light=algoSquare.light,
+                                           dark=algoSquare.dark, )
+                    # Есть все результаты, рендер их
+
 
         except FileNotFoundError:
             flash('Выберите изображение')
@@ -148,28 +174,29 @@ def index():
         except ValueError:
             savePath = calculatePathToSavedFile(session['idSession'])
             flash('Некорректное значение границ')
-            if not 'squareNumber' in session: # Если нет площадей
+            if not 'squareNumber' in session:  # Если нет площадей
                 return render_template("index.html", contours=savePath, light=algoSquare.light, dark=algoSquare.dark,
                                        Res=session['fractalNumber'])  # Рендер с фрактальной
-            else: # Есть все результаты
+            else:  # Есть все результаты
                 return render_template("index.html", contours=savePath, light=algoSquare.light, dark=algoSquare.dark,
-                                       Res=session['fractalNumber'], S=session['squareNumber']) # Есть все результаты, рендер их
+                                       Res=session['fractalNumber'],
+                                       S=session['squareNumber'])  # Есть все результаты, рендер их
         except Exception:
             flash('Сначала загрузите изображение')
             return render_template("index.html", light=algoSquare.light, dark=algoSquare.dark)
 
+
         else:
 
-            return render_template("index.html") # Отрисовка веб-страницы
+            return render_template("index.html")  # Отрисовка веб-страницы
     else:
         files = os.listdir(UPLOAD_FOLDER)
-        if len(files) > 150: # Уже число близкое к реальному (Количество сейвов 200)
+        if len(files) > 150:  # Уже число близкое к реальному (Количество сейвов 200)
             shutil.rmtree(UPLOAD_FOLDER)
             os.mkdir(UPLOAD_FOLDER)
         return render_template("index.html", light=algoSquare.light, dark=algoSquare.dark)  # Отрисовка веб-страницы
 
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, host='0.0.0.0') # Запуск веб-приложения, с многопоточностью и с возможностью
+    app.run(debug=True, threaded=True, host='0.0.0.0')  # Запуск веб-приложения, с многопоточностью и с возможностью
     # слушать все внешние ip
-
